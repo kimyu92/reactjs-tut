0b2184bfcf2c72aa290d9e6f3a56913fc32dbfba
Kim Yu Ng <kimyu_92@hotmail.com>
2015-10-04 21:21:17 -0500

    exploring state with dropdown done

--- tut6-dropdown/src/dropdown.jsx
+++ tut6-dropdown/src/dropdown.jsx
@@ -11,25 +11,43 @@ module.exports = React.createClass({
 
     // this.state.open = false; //this pattern should not exist in React
   },
+
   getInitialState() {
       return {
         open: false
       };
   },
-  render: function() {
-    var list = this.props.items.map(function(item){
-      return <ListItem item={item} />
+
+  handleItemClick(item) {
+    this.setState({
+      open: false,
+      itemTitle: item
     });
 
+    console.log(item);
+  },
+
+  render: function() {
+    var list = this.props.items.map(function(item){
+      return <ListItem 
+                item={item}
+                whenItemClicked={this.handleItemClick} 
+                className={this.state.itemTitle === item ? "active" : ""}
+              />
+    }.bind(this));
+
     return <div className="dropdown" > 
-      <Button whenClicked={this.handleClick}
-        buttonClassName="btn-default"
-        title={this.props.title}
-        subtitleClassName="caret"
-      />
-      <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
-        {list}
-      </ul>
-    </div>
+      
+            <Button whenClicked={this.handleClick}
+              buttonClassName="btn-default"
+              title={this.state.itemTitle || this.props.title}
+              subtitleClassName="caret"
+            />
+
+            <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
+              {list}
+            </ul>
+            
+          </div>
   }
 });
--- tut6-dropdown/src/list-item.jsx
+++ tut6-dropdown/src/list-item.jsx
@@ -1,8 +1,16 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  handleClick: function() {
+    return this.props.whenItemClicked(this.props.item);
+    //instead on putting the line above on onClick
+    //having handleClick callback is good for debugging
+    //it is also a proxy method
+  },
   render: function() {
-    return <li><a>{this.props.item}</a></li>
+    return <li className={this.props.className}>
+              <a onClick={this.handleClick} >{this.props.item}</a>
+          </li>
   }
 });
 
