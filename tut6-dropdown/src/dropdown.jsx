5a1165d76a91159b54f67812be4b7bdbc38701d0
Kim Yu Ng <kimyu_92@hotmail.com>
2015-10-04 20:56:52 -0500

    show the use of state in dropdown toggle

--- tut6-dropdown/src/dropdown.jsx
+++ tut6-dropdown/src/dropdown.jsx
@@ -4,9 +4,18 @@ var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
-    alert('kimchi');
-  },
+    //the state will be changed and it will rerender
+    this.setState({
+      open: !this.state.open
+    });
 
+    // this.state.open = false; //this pattern should not exist in React
+  },
+  getInitialState() {
+      return {
+        open: false
+      };
+  },
   render: function() {
     var list = this.props.items.map(function(item){
       return <ListItem item={item} />
@@ -18,7 +27,7 @@ module.exports = React.createClass({
         title={this.props.title}
         subtitleClassName="caret"
       />
-      <ul className="dropdown-menu show">
+      <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
         {list}
       </ul>
     </div>
